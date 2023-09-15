8223bf1cedfd89aaed118bf17a53752adbee5b85
wintiger0222 <0xodbs02@naver.com>
2023-09-15 15:05:15 +0900

    Seperate folders

--- compile.bat
+++ compile.bat
@@ -66,25 +66,25 @@ echo install menu
 asar   menu_text/_MenuFontInstall.asm fe4_trans.sfc 
 armips menu_text/_MenuFontInstall_armips.asm
 pause
-Atlas fe4_trans.sfc menu_text/misc_80.txt
-Atlas fe4_trans.sfc menu_text/misc_85.txt
-Atlas fe4_trans.sfc menu_text/misc_89.txt
-Atlas fe4_trans.sfc menu_text/misc_8A.txt
-Atlas fe4_trans.sfc menu_text/misc_8B.txt
-Atlas fe4_trans.sfc menu_text/misc_9E.txt
-Atlas fe4_trans.sfc menu_text/table_armyname.txt
-Atlas fe4_trans.sfc menu_text/table_castlename.txt
-Atlas fe4_trans.sfc menu_text/table_charname.txt
-Atlas fe4_trans.sfc menu_text/table_classname.txt
-Atlas fe4_trans.sfc menu_text/table_itemdesc.txt
-Atlas fe4_trans.sfc menu_text/table_itemname.txt
-Atlas fe4_trans.sfc menu_text/table_menu.txt
-Atlas fe4_trans.sfc menu_text/table_objective.txt
-Atlas fe4_trans.sfc menu_text/table_option.txt
-Atlas fe4_trans.sfc menu_text/table_optionchoices.txt
-Atlas fe4_trans.sfc menu_text/table_optionchoicepanel.txt
-Atlas fe4_trans.sfc menu_text/table_optiondesc.txt
-Atlas fe4_trans.sfc menu_text/table_skilldesc.txt
-Atlas fe4_trans.sfc menu_text/table_skillname.txt
-Atlas fe4_trans.sfc menu_text/table_terrain.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/misc_80.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/misc_85.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/misc_89.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/misc_8A.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/misc_8B.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/misc_9E.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_armyname.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_castlename.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_charname.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_classname.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_itemdesc.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_itemname.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_menu.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_objective.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_option.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_optionchoices.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_optionchoicepanel.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_optiondesc.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_skilldesc.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_skillname.txt
+Atlas fe4_trans.sfc menu_text/trans_2023/table_terrain.txt
 pause
